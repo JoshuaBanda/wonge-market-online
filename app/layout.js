7d49c0e5de8087066342f8b35e84fe1a48921c80
unimadatingapp <unimadatingapp@gmail.com>
2025-04-16 00:27:24 +0200

    ha

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,23 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "../Styles/globals.css";
-import BottomMenu from "./global/bottomMenu";
 import BottomMenuWrapper from "./global/BottomMenuWrapper";
+import { Roboto , Meow_Script,Lavishly_Yours,Lobster} from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Wonge Market Online",
   description: "Online Market",
 };
 
+const roboto= Lobster({
+  subsets:['latin'],
+  weight:'400',
+})
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={roboto.className }>
         <div  className="blur-wrapper">
           
         {children}
