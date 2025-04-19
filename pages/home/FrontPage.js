8a13647cff9577218793c658ad25185d89b11962
unimadatingapp <unimadatingapp@gmail.com>
2025-04-19 16:55:34 +0200

    h

--- pages/home/FrontPage.js
+++ pages/home/FrontPage.js
@@ -1,14 +1,34 @@
 import Header from "./Header";
 import HomePage from "./Home";
+import '../../Styles/globals.css'
 
+
+import {Pirata_One,Amaranth} from "next/font/google";
+
+
+export const metadata = {
+  title: "Wonge Market Online",
+  description: "Online Market",
+};
+
+const roboto= Amaranth({
+  subsets:['latin'],
+  weight:'400',
+});
 const FrontPage = () => {
     return (
-        <>
-        
-      <Header/>
-      <HomePage/>
-
-        </>
+      <html lang="en">
+        <body className={roboto.className }>
+          <div  className="blur-wrapper">
+            
+          <Header/>
+          <HomePage/>
+          
+  
+          </div>
+          
+        </body>
+      </html>
     );
 }
  
