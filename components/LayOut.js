314acb2b8ab6381a5a60636935ca6111f585ecc0
unimadatingapp <unimadatingapp@gmail.com>
2025-04-19 22:16:34 +0200

    h

--- components/LayOut.js
+++ components/LayOut.js
@@ -1,15 +1,29 @@
-import BottomMenuWrapper from "./BottomMenuWrapper";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../app/globals.css";
+import BottomMenuWrapper from "../components/BottomMenuWrapper";
+import {Pirata_One,Amaranth} from "next/font/google";
 
-const LayOut = ({children}) => {
-    return ( 
-        <html>
-                
-            <body>
-            {children}
-            <BottomMenuWrapper/>
-            </body>
-        </html> 
-    );
+
+export const metadata = {
+  title: "Wonge Market Online",
+  description: "Online Market",
+};
+
+const roboto= Amaranth({
+  subsets:['latin'],
+  weight:'400',
+})
+
+export default function LayOut({children}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className }>
+        <div  className="blur-wrapper">
+          {children}
+
+        </div>
+        <BottomMenuWrapper/>
+      </body>
+    </html>
+  );
 }
- 
-export default LayOut;
